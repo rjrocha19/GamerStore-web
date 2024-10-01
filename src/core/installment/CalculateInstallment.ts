28687835291eab8d301caa27d184117aa2b8fb44
rjrocha19 <rodolfo_rj@live.com>
2024-10-01 07:10:29 -0300

    update calculateInstallment core

--- src/core/installment/CalculateInstallment.ts
+++ src/core/installment/CalculateInstallment.ts
@@ -1,17 +1,43 @@
-import type Installment from '@/core/installment/Installment'
-import { MAX_PARCELA_QUANTITY, MONTHLY_INTEREST_RATE } from '@/data'
+import { MAX_PARCELA_QUANTITY, MONTHLY_INTEREST_RATE } from "@/core/constants";
+import type Installment from "@/core/installment/Installment";
 
-export default class CalculateInstallment {
+export default class CalcularInstallment {
   execute(
-    valueTotal: number,
-    numberInstallment: number = MAX_PARCELA_QUANTITY,
-    interestRate: number = MONTHLY_INTEREST_RATE,
-
+    value: number,
+    amountInstallments: number = MAX_PARCELA_QUANTITY,
+    interestRate: number = MONTHLY_INTEREST_RATE
   ): Installment {
-    if (numberInstallment < 2 || numberInstallment > MAX_PARCELA_QUANTITY) {
-      throw new Error('Invalid number of installment')
+    if (amountInstallments < 2 || amountInstallments > MAX_PARCELA_QUANTITY) {
+      throw new Error(
+        `Quantidade de parcelas deve ser entre 2 e ${MAX_PARCELA_QUANTITY}`
+      );
     }
 
-    const totalWithInterest = this.CalculateTotalWithInterest\100
+    const totalWithInterest = this.calculateCompoundInterest(
+      value,
+      interestRate,
+      amountInstallments
+    );
+
+    return {
+      valueInstallment: this.withTwoDecimalPlaces(
+        totalWithInterest / amountInstallments
+      ),
+      totalValue: this.withTwoDecimalPlaces(totalWithInterest),
+      interestRate,
+      amountInstallments,
+    };
+  }
+
+  private calculateCompoundInterest(
+    totalValue: number,
+    monthlyFee: number,
+    amountInstallments: number
+  ) {
+    return totalValue * (1 + monthlyFee / 100) ** amountInstallments;
+  }
+
+  private withTwoDecimalPlaces(value: number): number {
+    return Math.round(value * 100) / 100;
   }
 }
