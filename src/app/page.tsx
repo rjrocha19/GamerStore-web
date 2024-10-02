9548576bf2433a37924c9cffc1e1f2daf5b320b7
rjrocha19 <rodolfo_rj@live.com>
2024-10-02 00:06:27 -0300

    update project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { produtos } from '@/core'
 
 export default function Home() {
   return (
-    <div className='grid grid-cols-4'>
+    <div className="grid grid-cols-4 container">
       {produtos.map(produto => (
         <ProdutoItem key={produto.id} produto={produto} />
       ))}
